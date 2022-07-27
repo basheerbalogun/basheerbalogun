528a5aa082f5d0a4ad1a81af4532def7c94b2f44
Basheer Olamilekan Balogun <bbalogun@afexnigeria.com>
2022-07-27 14:54:06 +0100

    latest-work

---
+++ ATS/week_8/django/blog/blogapp/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.conf import settings
+
+class Tag(models.Model):
+    name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete= models.CASCADE, related_name='author')
+    content = models.TextField()
+    image = models.ImageField(upload_to='', blank=True, null=True)
+    tags = models.ManyToManyField(Tag, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now= True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
